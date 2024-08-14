61a9ae553329234ea3eef915c35f4e42acdceed2
Divyanshu Agarwal <73509365+divyanshuagarwal-23@users.noreply.github.com>
2024-08-14 21:35:07 +0530

    Add files via upload
    backend code

---
+++ backend/src/test/java/io/javabrains/tinderaibackend/TinderAiBackendApplicationTests.java
@@ -0,0 +1,11 @@
+package io.javabrains.tinderaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiBackendApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
